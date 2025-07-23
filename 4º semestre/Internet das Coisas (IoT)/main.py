dc15563493312193978e3efba019f0d2eebf41b3
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2025-07-23 14:45:05 -0300

    [fix] exercicio blink

---
+++ 4º semestre/Internet das Coisas (IoT)/main.py
@@ -0,0 +1,11 @@
+from machine import Pin
+from utime import sleep
+
+print("Hello World, ESP32!") #sserá exibido no terminal
+
+led = Pin(15, Pin.OUT) #configurando led com ESP32
+while True:
+  led.on()
+  sleep(0.5)
+  led.off()
+  sleep(0.5)
